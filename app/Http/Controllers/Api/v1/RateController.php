8fb8601403aeadbd5f308674a4125a5842bc6582
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2021-09-24 05:24:23 +0300

    done with rates

--- app/Http/Controllers/Api/v1/RateController.php
+++ app/Http/Controllers/Api/v1/RateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\v1\General\AgeResource;
 use App\Http\Resources\Api\v1\General\StandardResource;
+use App\Http\Resources\Api\v1\General\UserRateMessageResource;
 use App\Http\Resources\Api\v1\Teacher\GroupStudentResource;
 use App\Models\Age;
 use App\Models\ContactUs;
@@ -12,6 +13,7 @@ use App\Models\Group;
 use App\Models\Manager;
 use App\Models\Standard;
 use App\Models\User;
+use App\Models\UserRateMessage;
 use App\Notifications\ContactUsNotification;
 use App\Rules\EmailRule;
 use App\Rules\StartWith;
@@ -20,7 +22,9 @@ use Illuminate\Support\Facades\Notification;
 
 class RateController extends Controller
 {
+
     public function post_rate(Request $request, $user_id)
     {
+        return apiSuccess(new UserRateMessageResource(UserRateMessage::init($request, $user_id)),'Rated Successfully');
     }
 }
--- app/Models/UserStandardRate.php
+++ app/Models/UserStandardRate.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserStandardRate extends Model
 {
+    protected $guarded = [];
+
     public function user_rate_message()
     {
         return $this->belongsTo(UserRateMessage::class, 'parent_id');
+    }
+
+    public function standard()
+    {
+        return $this->belongsTo(Standard::class, 'standard_id');
     }
 }
