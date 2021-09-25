0b83dda35939cb9c685babe5fcfc3d2b829333b4
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2021-09-25 23:54:20 +0300

    update

--- database/migrations/2020_07_20_162817_create_activity_log_table.php
+++ database/migrations/2020_07_20_162817_create_activity_log_table.php
@@ -12,14 +12,14 @@ class CreateActivityLogTable extends Migration
     public function up()
     {
         Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('log_name')->nullable();
             $table->text('description');
             $table->unsignedBigInteger('subject_id')->nullable();
             $table->string('subject_type')->nullable();
             $table->unsignedBigInteger('causer_id')->nullable();
             $table->string('causer_type')->nullable();
-            $table->json('properties')->nullable();
+            $table->longText('properties')->nullable();
             $table->timestamps();
 
             $table->index('log_name');
